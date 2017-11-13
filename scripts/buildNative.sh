50baccd40382ae36e6d4640261f28c334347f77e
oscarkramer <okramer@radiantblue.com>
2017-11-13 17:15:55 -0500

    Fixed to allow local build of plugin

--- scripts/buildNative.sh
+++ scripts/buildNative.sh
@@ -9,11 +9,15 @@
 #
 #     OSSIM_INSTALL_PREFIX or OSSIM_DEV_HOME
 #     MSP_HOME or CSM_HOME
+#     OPENCV_HOME
 #
 # OSSIM_DEV_HOME is used for cases where the build is being 
 # done in a development environment.
 
 #################################################################
+
+# Uncomment following line to debug script line by line:
+#set -x; trap read debug
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 pushd $SCRIPT_DIR/.. >/dev/null
@@ -23,11 +27,23 @@ popd >/dev/null
 mkdir -p $CMAKE_DIR/build
 pushd $CMAKE_DIR/build
 
+if [ -n "$OSSIM_INSTALL_PREFIX" ]; then
+   echo "27"
+   CMAKE_MODULE_PATH=$OSSIM_INSTALL_PREFIX/share/ossim/CMakeModules
+elif [ -n "$OSSIM_DEV_HOME" ]; then
+   echo "30"
+   CMAKE_MODULE_PATH=$OSSIM_DEV_HOME/ossim/cmake/CMakeModules
+else
+  echo "ERROR: Must have either OSSIM_INSTALL_PREFIX or OSSIM_DEV_HOME defined. Cannot continue."
+  popd; exit 1
+fi
+
 echo "buildNative.sh: "
 echo "  OSSIM_DEV_HOME       = $OSSIM_DEV_HOME"
 echo "  OSSIM_INSTALL_PREFIX = $OSSIM_INSTALL_PREFIX"
 echo "  MSP_HOME             = $MSP_HOME"
 echo "  CMAKE_DIR            = $CMAKE_DIR"
+echo "  CMAKE_MODULE_PATH    = $CMAKE_MODULE_PATH"
 
 echo; echo "Generate makefiles."
 cmake \
@@ -37,7 +53,7 @@ cmake \
 -DMSP_HOME=$MSP_HOME \
 -DOPENCV_HOME=$OPENCV_HOME \
 -DCMAKE_INSTALL_PREFIX=$CMAKE_DIR/install \
--DCMAKE_MODULE_PATH=$OSSIM_INSTALL_PREFIX/share/ossim/CMakeModules \
+-DCMAKE_MODULE_PATH=$CMAKE_MODULE_PATH \
 $CMAKE_DIR
 
 if [ $? != 0 ]; then
