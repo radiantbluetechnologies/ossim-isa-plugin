5949760081377a6caa502d5df84c95aeec6bb554
oscarkramer <okramer@radiantblue.com>
2017-11-13 09:56:55 -0500

    Fixed install

--- include/isa/Config.h
+++
@@ -1,9 +0,0 @@
-//**************************************************************************************************
-//
-//     OSSIM Open Source Geospatial Data Processing Library
-//     See top level LICENSE.txt file for license information
-//
-//**************************************************************************************************
-#define OSSIM_ISA_GIT_REVISION "cc624b441e702a348806e9042e1bcc112629a851"
-#define OSSIM_ISA_BUILD_DATE "20171109"
-#define OSSIM_HAS_MSP 1
